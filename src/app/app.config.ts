3a0cee7769c38223f993fb5e14336759d0ba6864
Cindy Marin Torres <cindymarintorres@hotmail.com>
2026-06-10 23:29:23 -0500

    fix(config): reemplazar zone detection por provideZonelessChangeDetection (Angular 21)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import {
   ApplicationConfig,
   provideZoneChangeDetection,
+  provideZonelessChangeDetection, // nombre correcto en Angular 21
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
@@ -13,7 +14,8 @@ import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    //provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(), //solo este, se quita el provideZoneChangeDetection
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideBrowserGlobalErrorListeners(),
